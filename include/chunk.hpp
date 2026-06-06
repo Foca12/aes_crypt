4dfaf0a1ab202a02c271f57d92b7148d46846567
Foca12 <matteo.massinelli.12@gmail.com>
2026-06-06 19:11:04 +0200

    adding include and test

---
+++ include/chunk.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "../src/chunk.hpp"
---
+++ include/types.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+class ByteChunk128;
+
+namespace types
+{
+  typedef std::vector<int> ilist;
+  typedef std::vector<ByteChunk128> bclist;
+}
